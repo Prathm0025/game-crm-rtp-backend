536e23ab2f880f74905c1523cc37fb45116503d2
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-10-29 17:37:53 +0530

    fix checkToggleForPlayer

--- src/dashboard/middleware/checkToggle.ts
+++ src/dashboard/middleware/checkToggle.ts
@@ -36,22 +36,26 @@ export const checkToggle = async (req: Request, res: Response, next: NextFunctio
 };
 
 
-export const checkToggleForPlayer = async () => {
-  const toggle = await Toggle.findOne();
+export const checkToggleForPlayer = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const toggle = await Toggle.findOne();
 
-  if (toggle?.availableAt) {
-    const now = new Date();
-    const availableAt = new Date(toggle.availableAt);
+    if (toggle?.availableAt) {
+      const now = new Date();
+      const availableAt = new Date(toggle.availableAt);
 
-    // Check if the current time is before the 'availableAt' time
-    if (now < availableAt) {
-      return { isUnderMaintenance: true, availableAt };
+      // Check if the current time is before the 'availableAt' time
+      if (now < availableAt) {
+        return res.status(503).json({ message: `Under Maintenance until ${availableAt}` });
+      }
+
+      // If the time has passed, reset 'availableAt' to null
+      toggle.availableAt = null;
+      await toggle.save();
     }
 
-    // If the time has passed, reset 'availableAt' to null
-    toggle.availableAt = null;
-    await toggle.save();
+    next(); // Proceed if the service is available
+  } catch (error) {
+    return res.status(500).json({ message: 'Error checking service status' });
   }
-
-  return { isUnderMaintenance: false };
 };
