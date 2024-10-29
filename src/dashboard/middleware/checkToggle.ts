883990654bb532f7d26c4ac9f0d1bd17203ba212
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-10-29 17:33:07 +0530

    fix toggle for player

--- src/dashboard/middleware/checkToggle.ts
+++ src/dashboard/middleware/checkToggle.ts
@@ -6,28 +6,52 @@ import { AuthRequest } from '../../utils/utils';
 export const checkToggle = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const _req = req as AuthRequest;
-    const { username, role } = _req.user;
+    const { role } = _req.user;
 
-    if (role === "player") {
-      const toggle = await Toggle.findOne();
+    // Apply this check only if the user's role is "player"
+    if (role !== "player") {
+      return next(); // Skip toggle check for non-player roles
+    }
 
-      if (toggle?.availableAt) {
-        const now = new Date();
-        const availableAt = new Date(toggle.availableAt);
+    const toggle = await Toggle.findOne();
 
-        // Check if the current time is before the 'availableAt' time
-        if (now < availableAt) {
-          return res.status(503).json({ message: `Under Maintenance until ${availableAt}` });
-        }
+    if (toggle?.availableAt) {
+      const now = new Date();
+      const availableAt = new Date(toggle.availableAt);
 
-        // If the time has passed, reset 'availableAt' to null
-        toggle.availableAt = null;
-        await toggle.save();
+      // Check if the current time is before the 'availableAt' time
+      if (now < availableAt) {
+        return res.status(503).json({ message: `Under Maintenance until ${availableAt}` });
       }
+
+      // If the time has passed, reset 'availableAt' to null
+      toggle.availableAt = null;
+      await toggle.save();
     }
 
     next(); // Proceed if the service is available
   } catch (error) {
     return res.status(500).json({ message: 'Error checking service status' });
   }
+};
+
+
+export const checkToggleForPlayer = async () => {
+  const toggle = await Toggle.findOne();
+
+  if (toggle?.availableAt) {
+    const now = new Date();
+    const availableAt = new Date(toggle.availableAt);
+
+    // Check if the current time is before the 'availableAt' time
+    if (now < availableAt) {
+      return { isUnderMaintenance: true, availableAt };
+    }
+
+    // If the time has passed, reset 'availableAt' to null
+    toggle.availableAt = null;
+    await toggle.save();
+  }
+
+  return { isUnderMaintenance: false };
 };
