4d27022195450b8466103dec84a67fa2ca303975
rohit <rohit@underpinservices.com>
2024-11-12 12:01:02 +0530

    FIX: SL-BB testData

--- src/game/testData.ts
+++ src/game/testData.ts
@@ -1,155 +1,438 @@
-
 export const gameData = [
+
   {
-    "id": "SL-ONE",
-    "isSpecial": true,
+    "id": "SL-BB",
     "matrix": {
-      "x": 1,
-      "y": 1
+      "x": 5,
+      "y": 3
     },
-    "bets": [
-      1,
-      2,
-      3,
-      4,
-      5
+    "linesApiData": [
+      [
+        1,
+        1,
+        1,
+        1,
+        1
+      ],
+      [
+        0,
+        0,
+        0,
+        0,
+        0
+      ],
+      [
+        2,
+        2,
+        2,
+        2,
+        2
+      ],
+      [
+        0,
+        1,
+        2,
+        1,
+        0
+      ],
+      [
+        2,
+        1,
+        0,
+        1,
+        2
+      ],
+      [
+        0,
+        0,
+        1,
+        0,
+        0
+      ],
+      [
+        2,
+        2,
+        1,
+        2,
+        2
+      ],
+      [
+        1,
+        2,
+        2,
+        2,
+        1
+      ],
+      [
+        1,
+        0,
+        0,
+        0,
+        1
+      ],
+      [
+        2,
+        0,
+        0,
+        0,
+        2
+      ],
+      [
+        0,
+        2,
+        2,
+        2,
+        0
+      ],
+      [
+        2,
+        1,
+        2,
+        1,
+        2
+      ],
+      [
+        0,
+        1,
+        0,
+        1,
+        0
+      ],
+      [
+        1,
+        1,
+        2,
+        1,
+        1
+      ],
+      [
+        1,
+        1,
+        0,
+        1,
+        1
+      ],
+      [
+        1,
+        0,
+        1,
+        0,
+        1
+      ],
+      [
+        1,
+        2,
+        1,
+        2,
+        1
+      ],
+      [
+        2,
+        1,
+        1,
+        1,
+        2
+      ],
+      [
+        0,
+        1,
+        1,
+        1,
+        0
+      ],
+      [
+        1,
+        0,
+        2,
+        0,
+        1
+      ],
+      [
+        2,
+        2,
+        0,
+        2,
+        2
+      ],
+      [
+        0,
+        0,
+        2,
+        0,
+        0
+      ],
+      [
+        0,
+        2,
+        0,
+        2,
+        0
+      ],
+      [
+        2,
+        0,
+        2,
+        0,
+        2
+      ],
+      [
+        1,
+        2,
+        0,
+        2,
+        1
+      ],
+      [
+        0,
+        1,
+        2,
+        2,
+        2
+      ],
+      [
+        2,
+        1,
+        0,
+        0,
+        0
+      ],
+      [
+        0,
+        0,
+        0,
+        1,
+        2
+      ],
+      [
+        2,
+        2,
+        2,
+        1,
+        0
+      ],
+      [
+        0,
+        0,
+        1,
+        2,
+        2
+      ]
     ],
-
-    "linesApiData": [],
-    "scatterPurple": {
+    "linesCount": [
+      1,
+      5,
+      15,
+      20,
+      25,
+      30
+    ],
+    "bets": [
+      0.0005,
+      0.0025,
+      0.005,
+      0.0125,
+      0.025,
+      0.0375,
+      0.05,
+      0.0625,
+      0.1,
+      0.15,
+      0.25,
+      0.5,
+      0.75,
+      1,
+      1.5,
+      2
+    ],
+    "freeSpin": {
       "isEnabled": true,
-      "topSymbolProbs": [0, 140, 130, 120, 120, 110, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-      //make sure special symbols and empty have 0
-      "symbolsProbs": [1, 140, 130, 120, 120, 110, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0], // probability of each symbol
-      //make sure special symbols have 0
-      "featureProbs": [0, 6, 40, 8] // probability of each feature . index : 0 - no feature, 1 - level up, 2 - booster ,3 - both levelup and booster
+      "LPValues":[2,3,4,5,7],
+      "LPProbs":[0.9,0.7,0.6,0.3,0.1]
     },
-    "scatterBlue": {
+    "coinsvalue": [1, 2, 5, 10, 15, 20, 25, 30, 35, 40],
+    "coinsvalueprob": [10, 20, 30, 40, 50, 10, 15, 25, 35, 55],
+    "bonus": {
+      "type": "spin",
+      "isEnabled": false,
+      "noOfItem": 8,
+      "payOut": [200, 100, 70, 50, 30, 20, 10, 5],
+      "payOutProb": [0.05, 0.5, 1, 3, 10, 20, 25, 39.4]
+    },
+    "jackpot":{
       "isEnabled": true,
-      "symbolsProbs": [25, 14, 13, 12, 12, 11, 11, 11, 11, 9, 9, 8, 5, 2, 0, 0, 0], // probability of each symbol
-      "featureProbs": [0, 40, 40, 40] // probability of each feature . index : 0 - no feature, 1 - level up, 2 - booster ,3 - both levelup and booster
+      "payout":[5000,500,250,50],
+      "payoutProbs":[0.05,0.5,10,15]
     },
-    "booster": {
-      "isEnabledSimple": true,
-      "isEnabledExhaustive": true,
-      "type": "",
-      "typeProbs": [1, 15, 15], // index : 0 - no booster, 1 - simple booster, 2 - exhaustive booster
-      "multiplier": [1, 2, 3, 5, 10, 15, 20, 25],// multiplier amt
-      "multiplierProbs": [9, 70, 40, 20, 10, 4, 3, 1], // multiplier probability
+    "gamble": {
+      "type": "card",
+      "isEnabled": false
     },
-    "levelUp": {
-      "isEnabled": true,
-      "level": [0, 1, 2, 3, 4, 5, 6, 7],//increment symbol amounts . 0 - no level up
-      "levelProbs": [1, 50, 40, 30, 20, 15, 14, 12],// increment symbol probability 
-    },
-    "joker": {
-      "isEnabled": true,
-      "payout": [50, 500, 5000],
-      "blueRound": [8, 70, 60, 150],// 0 - no matches , 1 - only one match ...
-      "greenRound": [8, 70, 60, 150],// 0 - no matches , 1 - only one match ...
-      "redRound": [8, 70, 60, 50],// 0 - no matches , 1 - only one match ...
-      // "blueRound": [100, 1, 100, 1, 100, 1, 100, 1, 100, 1, 100, 10], // all even including 0 is joker - 0,2,4,6,8,10
-      // "greenRound": [100, 1, 1, 100, 1, 1, 100, 1, 1, 100, 1, 1],//all numbers divisible by 3 including 0 is joker - 0,3,6,9
-      // "redRound": [100, 1, 1, 1, 1, 100, 1, 1, 1, 1, 100, 10],//all numbers divisible by 5 including 0 is joker - 0,5,10
-
-    },
-
     "Symbols": [
       {
-        //empty
-        "Name": "empty",
+        "Name": "0",
         "Id": 0,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 9,
-          "1": 9,
-          "2": 9,
-          "3": 9,
-          "4": 9
+          "0": 10,
+          "1": 10,
+          "2": 10,
+          "3": 10,
+          "4": 10
         },
-        "freeSpinCount": 0,
-        "payout": 0,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            150,
+            0
+          ],
+          [
+            40,
+            0
+          ],
+          [
+            10,
+            0
+          ]
+        ]
       },
       {
-        //banana
-        "Name": "banana",
+        "Name": "1",
         "Id": 1,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 9,
-          "1": 9,
-          "2": 9,
-          "3": 9,
-          "4": 9
+          "0": 10,
+          "1": 10,
+          "2": 10,
+          "3": 10,
+          "4": 10
         },
-        "freeSpinCount": 1,
-        "payout": 1,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            150,
+            0
+          ],
+          [
+            40,
+            0
+          ],
+          [
+            10,
+            0
+          ]
+        ]
       },
       {
-        //watermelon
-        "Name": "watermelon",
+        "Name": "2",
         "Id": 2,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 9,
-          "1": 9,
-          "2": 9,
-          "3": 9,
-          "4": 9
+          "0": 10,
+          "1": 10,
+          "2": 10,
+          "3": 10,
+          "4": 10
         },
-        "freeSpinCount": 1,
-        "payout": 1,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            150,
+            0
+          ],
+          [
+            40,
+            0
+          ],
+          [
+            10,
+            0
+          ]
+        ]
       },
       {
-        //cherry
-        "Name": "cherry",
+        "Name": "3",
         "Id": 3,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 9,
-          "1": 9,
-          "2": 9,
-          "3": 9,
-          "4": 9
+          "0": 10,
+          "1": 10,
+          "2": 10,
+          "3": 10,
+          "4": 10
         },
-        "freeSpinCount": 1,
-        "payout": 2,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            150,
+            0
+          ],
+          [
+            40,
+            0
+          ],
+          [
+            10,
+            0
+          ]
+        ]
       },
       {
-        //grapes
-        "Name": "grapes",
+        "Name": "4",
         "Id": 4,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 9,
-          "1": 9,
-          "2": 9,
-          "3": 9,
-          "4": 9
+          "0":14,
+          "1":14,
+          "2":14,
+          "3":14,
+          "4":14
         },
-        "freeSpinCount": 1,
-        "payout": 2,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            250,
+            0
+          ],
+          [
+            50,
+            0
+          ],
+          [
+            20,
+            0
+          ]
+        ]
       },
       {
-        //lemon
-        "Name": "lemon",
+        "Name": "5",
         "Id": 5,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0":14,
+          "1":14,
+          "2":14,
+          "3":14,
+          "4":14
         },
-        "freeSpinCount": 1,
-        "payout": 2,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            250,
+            0
+          ],
+          [
+            50,
+            0
+          ],
+          [
+            20,
+            0
+          ]
+        ]
       },
       {
-        //orange
-        "Name": "orange",
+        "Name": "6",
         "Id": 6,
-        "isSpecial": false,
         "reelInstance": {
           "0": 4,
           "1": 4,
@@ -157,121 +440,170 @@ export const gameData = [
           "3": 4,
           "4": 4
         },
-        "freeSpinCount": 1,
-        "payout": 4,
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            250,
+            0
+          ],
+          [
+            50,
+            0
+          ],
+          [
+            20,
+            0
+          ]
+        ]
       },
       {
-        //bell
-        "Name": "bell",
-        "Id": 7,
-        "isSpecial": false,
-        "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
-        },
-        "freeSpinCount": 2,
-        "payout": 5,
-      },
-      {
-        //bar
-        "Name": "bar",
-        "Id": 8,
-        "isSpecial": false,
-        "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
-        },
-        "payout": 10,
-        "freeSpinCount": 2,
-      },
-      {
-        //7
         "Name": "7",
+        "Id": 7,
+        "reelInstance": {
+          "0": 4,
+          "1": 4,
+          "2": 4,
+          "3": 4,
+          "4": 4
+        },
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            250,
+            0
+          ],
+          [
+            50,
+            0
+          ],
+          [
+            20,
+            0
+          ]
+        ]
+      },
+      {
+        "Name": "8",
+        "Id": 8,
+        "reelInstance": {
+          "0": 4,
+          "1": 4,
+          "2": 4,
+          "3": 4,
+          "4": 4
+        },
+        "useWildSub": true,
+        "useHeisenberg": false,
+        "multiplier": [
+          [
+            250,
+            0
+          ],
+          [
+            50,
+            0
+          ],
+          [
+            20,
+            0
+          ]
+        ]
+      },
+      {
+        "Name": "9",
         "Id": 9,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0":44,
+          "1":44,
+          "2":44,
+          "3":44,
+          "4":44
         },
-        "payout": 15,
-        "freeSpinCount": 2,
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
       },
-
       {
-        //double bar
-        "Name": "doubleBar",
+        "Name": "10",
         "Id": 10,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0":54,
+          "1":54,
+          "2":54,
+          "3":54,
+          "4":54
         },
-        "freeSpinCount": 2,
-        "payout": 20,
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
       },
       {
-        //double 7
-        "Name": "double7",
+        "Name": "11",
         "Id": 11,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0": 45,
+          "1": 45,
+          "2": 45,
+          "3": 45,
+          "4": 45,
         },
-        "freeSpinCount": 2,
-        "payout": 30,
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
       },
-
       {
-        //triple bar
-        "Name": "tripleBar",
+        "Name": "12",
         "Id": 12,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0": 55,
+          "1": 55,
+          "2": 55,
+          "3": 55,
+          "4": 55
         },
-        "freeSpinCount": 0,
-        "payout": 50,
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
       },
-
       {
-        //triple 7
-        "Name": "triple7",
+        "Name": "13",
         "Id": 13,
-        "isSpecial": false,
         "reelInstance": {
-          "0": 4,
-          "1": 4,
-          "2": 4,
-          "3": 4,
-          "4": 4
+          "0": 14,
+          "1": 14,
+          "2": 14,
+          "3": 14,
+          "4": 14
         },
-        "freeSpinCount": 0,
-        "payout": 200,
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
       },
       {
-        "Name": "ScatterBlue",
+        "Name": "14",
         "Id": 14,
-        "isSpecial": true,
+        "reelInstance": {
+          "0":14,
+          "1":14,
+          "2":14,
+          "3":14,
+          "4":14
+        },
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+        ]
+      },
+      {
+        "Name": "Wild",
+        "Id": 15,
         "reelInstance": {
           "0": 5,
           "1": 5,
@@ -279,41 +611,119 @@ export const gameData = [
           "3": 5,
           "4": 5
         },
-        "description": "Scatter: Respin free games",
-        "freeSpinCount": 0,
-        "payout": 0,
-      },
-      {
-        "Name": "ScatterPurple",
-        "Id": 15,
-        "isSpecial": true,
-        "reelInstance": {
-          "0": 5,
-          "1": 5,
-          "2": 5,
-          "3": 5,
-          "4": 5,
-        },
-        "description": "Scatter: fruit free games",
-        "freeSpinCount": 0,
-        "payout": 0,
-      },
+        "description": "Substitutes for all symbols except Jackpot, Free Spin, Bonus, and Scatter.",
+        "useWildSub": false,
+        "useHeisenberg": false,
 
+        "multiplier": [
+
+        ]
+      },
       {
-        "Name": "Joker",
+        "Name": "Link",
         "Id": 16,
-        "isSpecial": true,
+        "reelInstance": {
+          "0": 0,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+          "4": 0
+        },
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": false,
+        "multiplier": [
+
+        ]
+      },
+      {
+        "Name": "MegaLink",
+        "Id": 17,
+        "reelInstance": {
+          "0": 0,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+          "4": 0
+        },
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": false,
+        "multiplier": [
+
+        ]
+      },
+      {
+        "Name": "CashCollect",
+        "Id": 18,
         "reelInstance": {
           "0": 5,
-          "1": 5,
-          "2": 5,
-          "3": 5,
-          "4": 5,
+          "1": 10,
+          "2": 10,
+          "3": 10,
+          "4": 5
         },
-        "description": "Joker",
-        "freeSpinCount": 0,
-        "payout": 0,
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": false,
+        "multiplier": [
+
+        ]
+      },
+      {
+        "Name": "Coins",
+        "Id": 19,
+        "reelInstance": {
+          "0":10,
+          "1":10,
+          "2":10,
+          "3":10,
+          "4":10
+        },
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": true,
+        "multiplier": [
+
+        ]
+      },
+      {
+        "Name": "PrizeCoin",
+        "Id": 20,
+        "reelInstance": {
+          "0": 53,
+          "1": 53,
+          "2": 53,
+          "3": 53,
+          "4": 53
+        },
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": false,
+        "multiplier": [
+
+        ]
+      },
+      {
+        "Name": "LosPollos",
+        "Id": 21,
+        "reelInstance": {
+          "0": 8,
+          "1": 8,
+          "2": 8,
+          "3": 8,
+          "4": 8
+        },
+        "description": "",
+        "useWildSub": false,
+        "useHeisenberg": false,
+        "multiplier": [
+
+        ]
       },
     ]
   }
 ]
+/*
+we have a product in angular front and golang backend . it is a task managment , crm , and chat app . they also want to include ai features in it . they want an assistant who will give the users an other to submit queries in natural language . these queries may be for example like  who has completed the most tasks ? who takes the least no of tasks? considering it is already a production ready app need plans and different approaches and pros and cons . 
+ * */
