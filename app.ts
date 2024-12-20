06c57ea183d52104c38d92f1b060d2e15abe7450
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-12-20 15:11:19 +0530

    convert company to supermaster

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
 import server from "./src/server";
 import connectDB from "./src/config/db";
 import { config } from "./src/config/config";
+import { User } from "./src/dashboard/users/userModel";
 const startServer = async () => {
 
   await connectDB();
   server.listen(config.port, () => {
     console.log("Listening on port : ", config.port);
   });
+
+  const result = await User.updateMany({ role: "company" }, { role: "supermaster" });
+  console.log("result", result);
+
 };
 
 startServer();
