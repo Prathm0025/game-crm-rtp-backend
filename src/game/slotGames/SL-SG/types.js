5bf4682a6f79646a480218bc653b5cf8da7c9736
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-01-31 10:31:43 +0000

    remove old artifacts

--- src/game/slotGames/SL-SG/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.specialIcons = void 0;
-var specialIcons;
-(function (specialIcons) {
-    specialIcons["wild"] = "Wild";
-    specialIcons["FreeSpin"] = "FreeSpin";
-})(specialIcons || (exports.specialIcons = specialIcons = {}));
