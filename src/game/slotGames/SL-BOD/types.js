f28f519a3f3479a4def2cf8ac098c93d0d2e9f90
TrippyRahul <TrippyRahul@users.noreply.github.com>
2025-02-08 06:50:52 +0000

    added: build artifacts

---
+++ src/game/slotGames/SL-BOD/types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.specialIcons = void 0;
+var specialIcons;
+(function (specialIcons) {
+    specialIcons["scatter"] = "Scatter";
+    specialIcons["expand"] = "Expand";
+})(specialIcons || (exports.specialIcons = specialIcons = {}));
