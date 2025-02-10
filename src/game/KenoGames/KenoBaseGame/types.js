84d42f114305ce7b0dc8aceea2fae2ce3eaa0a8f
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-02-10 07:23:57 +0000

    remove old artifacts

--- src/game/KenoGames/KenoBaseGame/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.specialIcons = void 0;
-var specialIcons;
-(function (specialIcons) {
-    specialIcons["wild"] = "Wild";
-    specialIcons["expand"] = "Expand";
-})(specialIcons || (exports.specialIcons = specialIcons = {}));
