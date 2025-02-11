2df41ecf986cbe8033d788fc97090f3b72a95957
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-02-11 12:16:11 +0000

    remove old artifacts

--- src/game/slotGames/SL-PSF/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.specialIcons = void 0;
-var specialIcons;
-(function (specialIcons) {
-    specialIcons["trumpFreeSpin"] = "TrumpFreeSpin";
-    specialIcons["wild"] = "Wild";
-    specialIcons["freeSpin"] = "FreeSpin";
-})(specialIcons || (exports.specialIcons = specialIcons = {}));
