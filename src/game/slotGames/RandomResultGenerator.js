f1885286f43c822fc316daae75c0d01cbcbc97b1
TrippyRahul <TrippyRahul@users.noreply.github.com>
2025-02-11 11:24:06 +0000

    added: build artifacts

---
+++ src/game/slotGames/RandomResultGenerator.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RandomResultGenerator = void 0;
+const gameUtils_1 = require("../Utils/gameUtils");
+class RandomResultGenerator {
+    constructor(current) {
+        let matrix = [];
+        for (let x = 0; x < current.settings.currentGamedata.matrix.x; x++) {
+            const startPosition = this.getRandomIndex((current.settings.reels[x].length - 1));
+            for (let y = 0; y < current.settings.currentGamedata.matrix.y; y++) {
+                if (!matrix[y])
+                    matrix[y] = [];
+                matrix[y][x] = current.settings.reels[x][(startPosition + y) % current.settings.reels[x].length];
+            }
+        }
+        // matrix.pop();
+        // matrix.pop();
+        // matrix.pop();
+        // matrix.push(['11', '11', '11', '11', '11'])
+        // matrix.push(['5', '3', '2', '1', '1'])
+        // matrix.push(['5', '3', '1', '5', '1'])
+        matrix.forEach(row => console.log(row.join(' ')));
+        current.settings.resultReelIndex = matrix;
+        current.settings.resultSymbolMatrix = matrix;
+    }
+    getRandomIndex(maxValue) {
+        let seed = Date.now() + Math.random() * 1000;
+        // return Math.floor(generateRandomNumber(seed, (maxValue + 1)));
+        return Math.floor((0, gameUtils_1.generatetrueRandomNumber)(maxValue + 1));
+    }
+}
+exports.RandomResultGenerator = RandomResultGenerator;
