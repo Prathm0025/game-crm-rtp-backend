1421afe7dbfb69230f92418b41672fff669a02d3
rana-fullstackdev <ranasingh@Rana-ka-Mac-mini-2.local>
2025-02-19 17:06:02 +0530

    FIX:wild substitution

--- src/game/slotGames/RandomResultGenerator.ts
+++ src/game/slotGames/RandomResultGenerator.ts
@@ -11,22 +11,12 @@ export class RandomResultGenerator {
       }
     }
 
-<<<<<<< HEAD
-
-    matrix.pop();
-    matrix.pop();
-    matrix.pop();
-    matrix.push(['0', '6', '2'])
-    matrix.push(['5', '3', '2'])
-    matrix.push(['5', '3', '1'])
-=======
     // matrix.pop();
     // matrix.pop();
     // matrix.pop();
     // matrix.push(["7", "0", "8", "1", "4"])
     // matrix.push(["2", "1", "1", "5", "6"])
     // matrix.push(["1", "2", "3", "9", "2"])
->>>>>>> 2f9a3ef733567bba0509441578c6bc02fda2220f
     matrix.forEach(row => console.log(row.join(' ')));
     current.settings.resultReelIndex = matrix;
     current.settings.resultSymbolMatrix = matrix;
