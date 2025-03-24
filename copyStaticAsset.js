2b862f4a271b5fa71ecc620ab960ec50aaf7a5e4
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-03-24 09:30:02 +0000

    remove old artifacts

--- copyStaticAsset.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs-extra");
-const path = require("path");
-
-const srcDir = path.join(__dirname, "src", "public");
-const destDir = path.join(__dirname, "dist", "src", "public");
-
-fs.copy(srcDir, destDir)
-  .then(() => console.log("✅ Static assets copied successfully!"))
-  .catch(err => console.error("❌ Error copying static assets:", err));
