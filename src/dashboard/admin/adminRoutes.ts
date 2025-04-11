5db2a4d6fc0eedc38cc638548e9dd52b58145b63
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-04-11 09:56:05 +0000

    remove old artifacts

--- src/dashboard/admin/adminRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { AdminController } from '../admin/adminController'
-const adminRoutes = express.Router();
-const company = new AdminController();
-
-adminRoutes.post('/request-otp', company.requestOTP)
-adminRoutes.post('/verify-otp', company.verifyOTPAndCreateUser)
-export default adminRoutes;
